2fbd3072fb18f83e06eae0ca63d1686877e2dba5
yousuf khann <mykhann10@gmail.com>
2026-01-24 15:37:02 +0500

    feat: create User schema with name, email, and password fields

---
+++ backend/model/user.model.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose"
+const schema= mongoose.Schema;
+
+const userSchema= new schema ({
+    name:{
+        type:String,
+        required:true
+    },
+    username:{
+        type:String,
+        required:true
+    },
+    password:{
+        type:String,
+        required:true 
+    },
+    tasks:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"Task"
+    },
+   
+
+
+},{timestamps:true})
+
+export default mongoose.model("User",userSchema)
