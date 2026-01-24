3f7e50d614e45e19e2fd1dc5ec1ef4a9607cac57
yousuf khann <mykhann10@gmail.com>
2026-01-24 15:29:01 +0500

    feat: add Task schema with completed, priority, and dueDate

---
+++ backend/model/task.model.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+const schema = mongoose.Schema;
+
+
+const taskSchema = new schema({
+    title: {
+        type: String,
+        required: true,
+        trim:true
+    },
+    description: {
+        type: String,
+        trim:true
+    },
+    postedBy: {
+        type: mongoose.Types.ObjectId,
+        ref: "User"
+    },
+    completed: {
+        type: Boolean,
+        default: false
+    },
+    priority: {
+        type: String,
+        enum: ["low", "medium", "high"]
+    },
+    dueDate: Date
+
+}, { timestamps: true })
+
+
+export default mongoose.model("Task", taskSchema)
