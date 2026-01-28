2462ff73b58ba4ebb434c40a320e9e99dac1e401
yousuf khann <mykhann10@gmail.com>
2026-01-29 03:41:04 +0500

    feat : add update task controller

--- backend/controller/task.controller.js
+++ backend/controller/task.controller.js
@@ -3,7 +3,7 @@ import Task from "../model/task.model.js"
 
 export const postTask = AsyncHandler(async (req, res) => {
     const { title, description, priority } = req.body;
-    const {userId}=req.params
+    const { userId } = req.params
 
     if (!title) {
         return res.status(400).json({
@@ -15,36 +15,58 @@ export const postTask = AsyncHandler(async (req, res) => {
         title: title,
         description: description,
         priority: priority,
-        completed:false,
-        postedBy:req.user._id,
-        priority:priority,
-        assignedTo:userId
+        completed: false,
+        postedBy: req.user._id,
+        priority: priority,
+        assignedTo: userId
     })
 
     res.status(200).json({
-        message:"Task created successfully",
-        success:true,
+        message: "Task created successfully",
+        success: true,
         task
     })
 
 
 })
 
-export const getLoggedInUserTasks=AsyncHandler(async(req,res)=>{
-    const loggedInUserId=req.user._id;
-    const loggedInUserTasks=await Task.find({assignedTo:loggedInUserId}).populate("postedBy" ,"name")
-    if (loggedInUserTasks.length === 0){
+export const getLoggedInUserTasks = AsyncHandler(async (req, res) => {
+    const loggedInUserId = req.user._id;
+    const loggedInUserTasks = await Task.find({ assignedTo: loggedInUserId }).populate("postedBy", "name")
+    if (loggedInUserTasks.length === 0) {
         return res.status(404).json({
-            success:false,
-            message:"No Task found"
+            success: false,
+            message: "No Task found"
+        })
+    }
+    res.status(200).json({
+        success: true,
+        message: "All tasks found",
+        loggedInUserTasks
+
+    })
+
+
+})
+
+export const updageteTaskStatus = AsyncHandler(async (req, res) => {
+    const { taskId } = req.params
+    const { completed } = req.body
+
+    const task = await Task.findByIdAndUpdate(
+        taskId,
+        { completed },
+        { new: true, runValidators: true }).lean().select(" completed ")
+    if (!task) {
+        return res.status(404).json({
+            success: false,
+            message: "Task not found"
         })
     }
     res.status(200).json({
         success:true,
-        message:"All tasks found",
-        loggedInUserTasks
-
+        message:"status updated successfully",
+        task
     })
-   
-    
+
 })
--- backend/route/task.route.js
+++ backend/route/task.route.js
@@ -1,8 +1,10 @@
 import express from "express"
 const router = express.Router()
-import { getLoggedInUserTasks, postTask } from "../controller/task.controller.js";
+import { getLoggedInUserTasks, postTask, updageteTaskStatus } from "../controller/task.controller.js";
 import isAuthenticated from "../middleware/isAuthenticated.js";
 
-router.post("/create/:userId", isAuthenticated, postTask)
+
 router.get("/get", isAuthenticated, getLoggedInUserTasks)
+router.post("/create/:userId", isAuthenticated, postTask)
+router.patch("/update/:taskId", isAuthenticated, updageteTaskStatus)
 export default router;
